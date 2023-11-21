5c7113806cd907b1f4a47bcb837bb685b379c494
Haneef4128 <114045201+Haneef4128@users.noreply.github.com>
2023-11-21 08:54:22 +0530

    corners using harris corner

--- exp4.py
+++ exp4.py
@@ -1,13 +1,25 @@
 import cv2
 import numpy as np
-kernel = np.ones((5,5),np.uint8)
-print(kernel)
-path = "cv.png"
-img =cv2.imread(path)
-imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-imgBlur = cv2.GaussianBlur(imgGray,(7,7),0)
-imgCanny = cv2.Canny(imgBlur,100,200)
-imgDilation = cv2.dilate(imgCanny,kernel , iterations = 10)
-imgEroded = cv2.erode(imgDilation,kernel,iterations=2)
-cv2.imshow("Img Erosion",imgEroded)
+
+# Load the image
+img = cv2.imread('cv.png')
+
+# Convert image to float32
+img_float32 = np.float32(img)
+
+# Convert the image to grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+# Detect corners using Harris Corner Detection
+dst = cv2.cornerHarris(gray, 2, 3, 0.04)
+
+# Dilate corners to mark them
+dst = cv2.dilate(dst, None)
+
+# Mark the corners on the original image
+img[dst > 0.01 * dst.max()] = [0, 0, 255]
+
+# Display the corners
+cv2.imshow('Corners', img)
 cv2.waitKey(0)
+cv2.destroyAllWindows()
