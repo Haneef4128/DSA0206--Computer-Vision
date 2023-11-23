a2c1a5368b62f9e53eb8a5c52b4286aef31e2b56
Haneef4128 <114045201+Haneef4128@users.noreply.github.com>
2023-11-23 12:47:21 +0530

    white image with borders

--- exp29.py
+++ exp29.py
@@ -1,23 +1,46 @@
 import cv2
 import numpy as np
 
-def create_image_with_boxes(size):
+def create_white_image_with_boxes():
+    # Get size of the image from the user
+    width = int(input("Enter the width of the image: "))
+    height = int(input("Enter the height of the image: "))
+
     # Create a white image of the specified size
-    white_image = np.full((size, size, 3), 255, dtype=np.uint8)
+    img = np.ones((height, width, 3), np.uint8) * 255
 
-    # Calculate the size of the colored boxes
-    box_size = size // 10
+    # Draw four boxes on the image
+    box_size = int(min(width, height) / 10)  # Size of the colored boxes
+    thickness = -1  # Fill the boxes by the specified color
 
-    # Create black, blue, green, and red boxes on each corner of the image
-    white_image[0:box_size, 0:box_size] = [0, 0, 0]  # Black box in the top-left corner
-    white_image[0:box_size, size - box_size:size] = [255, 0, 0]  # Blue box in the top-right corner
-    white_image[size - box_size:size, 0:box_size] = [0, 255, 0]  # Green box in the bottom-left corner
-    white_image[size - box_size:size, size - box_size:size] = [0, 0, 255]  # Red box in the bottom-right corner
+    # Draw black box on the top-left corner
+    start_point = (0, 0)
+    end_point = (box_size, box_size)
+    color = (0, 0, 0)  # BGR color format (blue, green, red)
+    cv2.rectangle(img, start_point, end_point, color, thickness)
+
+    # Draw blue box on the top-right corner
+    start_point = (width - box_size, 0)
+    end_point = (width, box_size)
+    color = (255, 0, 0)  # BGR color format (blue, green, red)
+    cv2.rectangle(img, start_point, end_point, color, thickness)
+
+    # Draw green box on the bottom-left corner
+    start_point = (0, height - box_size)
+    end_point = (box_size, height)
+    color = (0, 255, 0)  # BGR color format (blue, green, red)
+    cv2.rectangle(img, start_point, end_point, color, thickness)
+
+    # Draw red box on the bottom-right corner
+    start_point = (width - box_size, height - box_size)
+    end_point = (width, height)
+    color = (0, 0, 255)  # BGR color format (blue, green, red)
+    cv2.rectangle(img, start_point, end_point, color, thickness)
 
     # Display the image
-    cv2.imshow('Image with Boxes', white_image)
+    cv2.imshow("White Image with Boxes", img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-# Call the function with the desired size (e.g., 400)
-create_image_with_boxes(400)
+# Call the function to create the white image with four boxes
+create_white_image_with_boxes()
